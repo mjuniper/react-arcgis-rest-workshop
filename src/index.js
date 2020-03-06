6735475998389b3228f2b5dc517f79e0f04d1b17
mjuniper <mjuniper@gmail.com>
2020-03-06 13:13:56 -0700

    deployment is working

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+let Router = HashRouter;
+let basename;
+if (process.env.NODE_ENV === 'development') {
+  Router = BrowserRouter;
+  basename = '/react-arcgis-rest-workshop';
+}
+
+ReactDOM.render(<Router basename={basename}><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
